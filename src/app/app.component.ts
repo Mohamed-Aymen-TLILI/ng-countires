ce88013870db8aa2d44bfd46afc4a1c80a187c04
libra1919 <aymentli@gmail.com>
2020-12-10 23:34:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ApiService } from './services/api.service';
+import { ThemeService, Theme } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'ng-countries';
+export class AppComponent implements OnInit {
+  theme: Observable<Theme>;
+
+  constructor(
+    private themeService: ThemeService
+  ) {}
+
+  ngOnInit() {
+    this.theme = this.themeService.mode$;
+  }
 }
